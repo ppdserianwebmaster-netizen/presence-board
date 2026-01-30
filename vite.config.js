89ae268621b58b5a8a834ea668f2a986399f73e0
ppdserianwebmaster-netizen <ppdserian.webmaster@gmail.com>
2026-01-30 18:38:04 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,44 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+// import {
+//     defineConfig
+// } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//         tailwindcss(),
+//     ],
+//     server: {
+//         cors: true,
+//         watch: {
+//             ignored: ['**/storage/framework/views/**'],
+//         },
+//     },
+// });
+
+import {  
+    defineConfig  
+} from 'vite';  
+import laravel from 'laravel-vite-plugin';  
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        cors: true,
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
-});
+export default defineConfig({  
+    base: '/build/',  
+    plugins: [  
+        laravel({  
+            input: ['resources/css/app.css', 'resources/js/app.js'],  
+            refresh: true,  
+        }),  
+        tailwindcss(),  
+    ],  
+    server: {  
+        cors: true,  
+        watch: {  
+            ignored: ['**/storage/framework/views/**'],  
+        },  
+    },  
+});  
