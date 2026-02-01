9040ca17cd3f0b09d3bdc54bd19d44c2d0bd2bad
ppdserianwebmaster-netizen <ppdserian.webmaster@gmail.com>
2026-02-01 09:11:20 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -71,8 +71,8 @@ return [
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
             'options' => extension_loaded('pdo_mysql') ? array_filter([  
-                PDO::MYSQL_ATTR_SSL_MODE => 'DISABLED',  
-            ]) : [],
+                'ssl_mode' => 'DISABLED',  
+            ]) : [],  
         ],
 
         'mariadb' => [
